1432c119c0131a641d4b94d3397183dd1125f7ab
Muhammad Sohail <sohailsadiqui001@gmail.com>
2026-08-05 23:47:58 +0500

    Add if-else structure for grading system

---
+++ If_else.js
@@ -0,0 +1,9 @@
+let marks = Number(prompt("Enter your marks (0-100):"));
+
+if (marks > 90) {
+    console.log("Excellent");
+} else if (marks >= 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
